2df59b8c76887d40e939b22f0ca188ad387073b9
RightShiner <brilight.shiner@gmail.com>
2023-04-03 09:54:47 +0200

    feat: Data Binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,10 +6,12 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = "Lars";
+    public $lazy = false;
+    public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', [
-            'name' => 'Lars'
-        ]);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,13 @@
 <div>
-  Hello {{ $name }}
+  <input wire:model="name" type="text">
+  <input wire:model="lazy" type="checkbox">
+  <select wire:model="greeting" multiple>
+    <option>Hello</option>
+    <option>Goodbye</option>
+    <option>Hi</option>
+  </select>
+  {{ implode(', ', $greeting) }} {{ $name }}
+  @if ($lazy)
+    !
+  @endif
 </div>
