5ed4c82c396adf6c596ec7c1e9b972f563cff8f3
RightShiner <brilight.shiner@gmail.com>
2023-04-03 11:16:54 +0200

    feat: Lifecycle hooks

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -1,14 +1,22 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class HelloWorld extends Component
 {
     public $name = "Lars";
-    public $lazy = false;
-    public $greeting = ['Hello'];
+
+    public function mount($name)
+    {
+        $this->name = $name;
+    }
+
+    public function updated($name)
+    {
+        $this->name = strtoupper($this->name);
+    }
 
     public function render()
     {
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,17 +1,5 @@
 <div>
   <input wire:model="name" type="text">
-  <input wire:model="lazy" type="checkbox">
-  <select wire:model="greeting" multiple>
-    <option>Hello</option>
-    <option>Goodbye</option>
-    <option>Hi</option>
-  </select>
-  {{ implode(', ', $greeting) }} {{ $name }}
-  @if ($lazy)
-    !
-  @endif
-  <form action="#" wire:submit.prevent="$set('name', 'Bingo')">
-    <button>Reset Name</button>
-  </form>
-  {{-- <button wire:click="resetName($event.target.innerText)">Reset Name</button> --}}
+
+  Hello {{ $name }}
 </div>
